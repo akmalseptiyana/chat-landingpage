ab5f27004c838dc8cae2226f53d94f37b33436a5
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-03-19 20:43:19 +0700

    fix: single quote

--- src/components/sections/faq.tsx
+++ src/components/sections/faq.tsx
@@ -60,7 +60,7 @@ const FAQ = [
       "Features available in the trial are parallel to those of the Team plan. You can add up to 100 agents. If you want to trial Chat N&apos; Rechat in another plan, simply start a chat with us and we’ll make it happen!",
   },
   {
-    question: "Where can I download Chat N&apos; Rechat?",
+    question: "Where can I download Chat N'Rechat?",
     answer:
       "You can download Chat N&apos; Rechat desktop apps from our Product page. We have apps available for Mac and Windows. If you need an application for a mobile device, you can download them either from the App Store or Google Play. We have apps for iPhone and Android.",
   },
